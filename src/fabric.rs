a28d44cbc96d74943479d734fb156058dbe35812
Jason Verbeek <jason@localhost8080.org>
2022-01-05 21:18:15 +0100

    Added comments to code

--- src/fabric.rs
+++ src/fabric.rs
@@ -13,6 +13,8 @@ pub enum FabricError {
 }
 
 impl FabricError {
+    // map enum to a basic error message
+    // NOTE that this doesn't allow for adding variables or custom text
     pub fn value(&self) -> &'static str {
         match *self {
             Self::NoFabricProject => "Current directory is not a fabric project",
@@ -40,14 +42,14 @@ pub struct Instruction {
 }
 
 impl Instruction {
+    // simple helper to do a check
     pub fn is_composed(&self) -> bool {
-        if let Some(_) = &self.subfabrics {
-            return true;
-        }
-        return false;
+        self.subfabrics.is_some()
     }
 
     pub fn explain(&self) -> String {
+        // helper to attempt to explain a fabric
+        // the fact I need to check vars to determine what kind it is somewhat wrong, there has to be something for this in Rust
         if let Some(subfabrics) = &self.subfabrics {
             return format!("composition of commands: {}", subfabrics.join(", "));
         } else if let (Some(command), Some(args)) = (&self.command, &self.args) {
@@ -65,15 +67,18 @@ pub struct Fabric {
 
 impl Fabric {
     pub fn load_project(project_file: &'_ str) -> Result<Self> {
+        // check if .fabric file exists
         if !Path::new(project_file).exists() {
             return Err(FabricError::NoFabricProject);
         }
 
+        // read .fabric file into memory
         let mut file = File::open(project_file).or(Err(FabricError::NoAccessToFile))?;
         let mut content = String::new();
         file.read_to_string(&mut content)
             .or(Err(FabricError::NoAccessToFile))?;
 
+        // decode json to a Fabric object/instance
         let fabric: Fabric =
             serde_json::from_str(content.as_str()).or(Err(FabricError::InvalidFabric))?;
 
@@ -92,14 +97,20 @@ impl Fabric {
     }
 
     pub fn find_by_name(&self, name: &String) -> Option<&Instruction> {
+        // search through all fabrics returning an Option with the matching instruction if found
         self.fabrics.iter().find(|f| f.name.eq(name))
     }
 
     fn expand_all(&self, tasks: &Vec<String>) -> Vec<&Instruction> {
+        // This function loops over all instructions and expands those that are subfabrics in the order of the args/tasks
+        // turning `subfabrics: ["a", "b"]` into 2 seperate tasks (a and b)
+
+        // map args/tasks into their respective Instructions
         let raw_instructions = tasks
             .into_iter()
             .filter_map(|t| -> Option<&Instruction> { return self.find_by_name(t) });
 
+        // build a vec in the order of the args/tasks with expanded subfabrics
         let mut expanded_instructions: Vec<&Instruction> = Vec::new();
         for ri in raw_instructions {
             if !ri.is_composed() {
@@ -110,38 +121,49 @@ impl Fabric {
                 }
             }
         }
+        //return that build vec
         expanded_instructions
     }
 
     pub fn expand(&self, instruction: &Instruction) -> Option<Vec<&Instruction>> {
+        // expand a single instruction which is a collection of instructions into a ordered list of that collection
+
+        // if instruction does not have subfrabrics return None because nothing can be expanded
         let subfabrics = match &instruction.subfabrics {
             None => return None,
             Some(subfabrics) => subfabrics,
         };
+        // build expanded vec of instruction based on the names of the subfabrics
         let mut expanded: Vec<&Instruction> = Vec::new();
         for sf in subfabrics {
             if let Some(instr) = self.find_by_name(&sf) {
                 expanded.push(instr);
             }
         }
+        // return result
         return Some(expanded);
     }
 
     pub fn execute_instruction(instruction: &Instruction) -> Result<()> {
+        // execute a single Instruction on the system
         if let (Some(command), Some(args)) = (&instruction.command, &instruction.args) {
+            // spawn process
             let mut cmd = Command::new(command)
                 .args(args)
                 .spawn()
                 .or(Err(FabricError::ExecutionError))?;
+            // check wether exit was successful, if not stop execution of all Instructions
             let exitcode = cmd.wait().or(Err(FabricError::ExecutionError))?;
             if !exitcode.success() {
                 return Err(FabricError::ExecutionError);
             }
         }
+        // execution finished
         Ok(())
     }
 
     pub fn execute_all(&self, raw_tasks: &Vec<String>) -> Result<()> {
+        // execute all instructions gathered based on the args/Tasks
         let tasks: Vec<&Instruction> = self.expand_all(raw_tasks);
         for task in tasks {
             println!(
--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,15 @@ fn fabricate() -> Result<()> {
     let fabric: Fabric = Fabric::load_project("./.fabric")?;
     let args: Vec<String> = env::args().skip(1).collect();
 
+    // basic args parsing
+    // no need for clap crate
+    // 0 args = help
+    // any args = attempt execute all args
     if args.len() == 0 {
         println!("Available commands:");
         println!("{} - show this help page", "fabric".bold());
         for instr in &fabric.fabrics {
+            // do not show help for private fabrics
             if &instr.private == &true {
                 continue;
             }
@@ -27,10 +32,12 @@ fn fabricate() -> Result<()> {
                 instr.explain()
             );
         }
+        // exit so we do not print the "done" message
         exit(0);
+    } else {
+        // start executing all args in order
+        fabric.execute_all(&args)?;
     }
-
-    fabric.execute_all(&args)?;
     Ok(())
 }
 
@@ -38,6 +45,7 @@ fn main() {
     println!("Fabric version {}", VERSION);
     println!();
 
+    // attempt to fabricate the project and collect/handle errors in one place
     match fabricate() {
         Ok(_) => {
             println!("{}: All fabrics completed!", "FIN".green().bold());
